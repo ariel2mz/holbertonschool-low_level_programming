97394a2d1030e0873ae586d11ed2edbcc31cbd8f
ariel <ariel2@gmail.com>
2024-06-13 13:36:00 +0000

    g

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    /* imprimir un string con puts*/
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+/* imprimir un string con puts*/
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
