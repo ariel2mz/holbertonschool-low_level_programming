8bd48c9dec9a94b5eac81fdf783771c7cf6a9884
ariel <ariel2@gmail.com>
2024-06-13 13:47:14 +0000

    aaa

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* imprimir un string con puts*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+/*retorna 0 porque no importa que retornar*/
+return (0);
+}
