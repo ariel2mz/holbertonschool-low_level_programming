ea342e7f1cdad65cd870e462684ec58b76d13fd3
ariel <ariel2@gmail.com>
2024-06-13 13:44:36 +0000

    gh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 /* imprimir un string con puts*/
 puts("\"Programming is like building a multilingual puzzle");
+/*retorna 0 porque no importa que retornar*/
 return (0);
 }
