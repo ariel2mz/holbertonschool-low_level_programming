582caf02d39af76f4bb869ff29c229b631af7b3a
ariel <ariel2trabajo@gmail.com>
2024-06-14 14:45:59 +0000

    56

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+putchar(i);
+putchar('\n');
+}
