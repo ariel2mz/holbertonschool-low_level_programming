228a6aca65478596939cba18e659bd943b1e79f3
ariel <ariel2trabajo@gmail.com>
2024-06-14 15:07:55 +0000

    sa

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+putchar('$');
+return (0);
+}
