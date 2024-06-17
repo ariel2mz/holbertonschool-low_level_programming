bdf7e3ad7904e3ae9b9fb6e3041263da8925bd39
ariel <ariel2trabajo@gmail.com>
2024-06-17 13:13:42 +0000

    aa

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-_putchar('p');   
+_putchar('_');   
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -17,5 +17,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
 return (0);
 }
