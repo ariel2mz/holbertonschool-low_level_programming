f13d5b986aa503ace11a56ac3cb20ca96558713f
ariel <ariel2trabajo@gmail.com>
2024-06-17 13:59:30 +0000

    sasa

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+int _islower(int c);
+{
+char r;
+
+for (r = 'a'; r <= 'z'; r++)
+{
+if (r == c)
+{
+return (1);
+}
+}
+}
