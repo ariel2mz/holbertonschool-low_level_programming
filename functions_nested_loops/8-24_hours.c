eff9004dae74400722a93ecb43a304935817701b
ariel <ariel2trabajo@gmail.com>
2024-06-17 16:14:32 +0000

    aaaaa

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@
     for (h1 = 0; h1 < 3; h1++){
         for (h2 = 0; h1 <= 9; h2++){
             for(m1 = 0; m1 <= 5; m1++){
-                for(m2 = 0; m1 <= 9; m1++){
+                for(m2 = 0; m2 <= 9; m1++){
                     _putchar(h1 + '0');
                     _putchar(h2 + '0');
                     _putchar(':');
