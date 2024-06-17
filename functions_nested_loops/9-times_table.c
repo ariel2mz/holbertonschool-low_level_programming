03d7eacc4056eebf842ad4e16b7de9a77a407295
ariel <ariel2trabajo@gmail.com>
2024-06-17 17:25:02 +0000

    kkk

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,13 +10,17 @@ void times_table(void){
     int i;
     int j;
     int k;
+    int r;
+    int d;
 
     for(i = 0; i <= 9; i++){
         for (j = 0; j <= 9; j++){
             k = i * j;
             if(k >= 10){
-            _putchar(k/10 + '0');
-            _putchar(k%10 + '0');
+                d = k/10;
+                r = k%10;
+            _putchar(d + '0');
+            _putchar(r + '0');
             }
             else
             _putchar(' ');
