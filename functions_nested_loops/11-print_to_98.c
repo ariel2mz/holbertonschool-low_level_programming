7e86e09e1379937cd92859b6b0f5c449b2d33c2e
ariel <ariel2trabajo@gmail.com>
2024-06-17 18:26:57 +0000

    aaa

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,16 @@
 /**
  * print_to_98- Entry point
  * Description: 'Escribe un texto'
- * @n: aa
+ * @n:
  * Return: int
  */
 void print_to_98(int n)
 {
 for (; n <= 98; n++)
 {
-printf("%d, ", n);
+printf("%d", n);
+if (n != 98)
+printf(", ");
 }
+_putchar('\n');
 }
