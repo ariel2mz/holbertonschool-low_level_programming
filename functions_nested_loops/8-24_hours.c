278c1e68f986d5b61e9086ad359d916d14798740
ariel <ariel2trabajo@gmail.com>
2024-06-17 17:04:30 +0000

    kkk

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,27 +6,31 @@
  * @i: caracter a revisar
  * Return: int
  */
- void jack_bauer(void){
-    int h1;
-    int h2;
-    int m1;
-    int m2;
+void jack_bauer(void)
+{
+int h1;
+int h2;
+int m1;
+int m2;
 
-    for (h1 = 0; h1 < 3; h1++){
-        for (h2 = 0; h2 <= 9; h2++){
-            for(m1 = 0; m1 <= 5; m1++){
-                for(m2 = 0; m2 <= 9; m2++){
-                    _putchar(h1 + '0');
-                    _putchar(h2 + '0');
-                    _putchar(':');
-                    _putchar(m1 + '0');
-                    _putchar(m2 + '0');
-                    _putchar('\n');
-                }
-            }
-        if (h1 == 2 && h2 == 3)
-        break;
-        }
-        
-    }
- }
+for (h1 = 0; h1 < 3; h1++)
+{
+for (h2 = 0; h2 <= 9; h2++)
+{
+for (m1 = 0; m1 <= 5; m1++)
+{
+for (m2 = 0; m2 <= 9; m2++)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
+_putchar(':');
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+}
+}
+if (h1 == 2 && h2 == 3)
+break;
+}
+}
+}
