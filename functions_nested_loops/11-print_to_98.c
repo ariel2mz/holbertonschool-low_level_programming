f68847648884076c40cbf88400f07926cdb26a1d
ariel <ariel2trabajo@gmail.com>
2024-06-17 18:21:34 +0000

    aaa

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * times_table- Entry point
  * Description: 'Escribe un texto'
  * Return: int
  */
-int add(int a, int b)
+void print_to_98(int n)
 {
-return (a + b);
+for (; n <= 98; n++)
+{
+printf("%d, ", n);
+}
 }
