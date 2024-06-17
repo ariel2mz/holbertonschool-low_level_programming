c12f22bc01e94340e2bcb8a089e75992f3983ed2
ariel <ariel2trabajo@gmail.com>
2024-06-17 17:38:09 +0000

    aaaaaaa

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -24,15 +24,29 @@ void times_table(void){
             _putchar(r + '0');
             _putchar(',');
             }
-            else if(k <= 9 && j != 0){
+            else if(k <= 9 && j != 0 && j != 9){
             _putchar(' ');
             _putchar(' ');
             _putchar(k +'0');
             _putchar(',');
             }
-            else{
+            else if(j == 0){
                 _putchar('0');
                 _putchar(',');
+            }
+            else{
+                if (k > 9){
+                    d = k/10;
+                r = k%10;
+            _putchar(' ');
+            _putchar(d + '0');
+            _putchar(r + '0');
+                }
+                else{
+                    _putchar(' ');
+            _putchar(' ');
+            _putchar(k +'0');
+                }
             }
         }
         _putchar('\n');
