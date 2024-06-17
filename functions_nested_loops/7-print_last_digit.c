165eb174d44f8cb5fd3654b25fea196c5beccd11
ariel <ariel2trabajo@gmail.com>
2024-06-17 15:47:22 +0000

    aa

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+/**
+ * print_last_digit- Entry point
+ *
+ * Description: 'Escribe un texto'
+ * @i: caracter a revisar
+ * Return: int
+ */
+int print_last_digit(int i)
+{
+int u;
+
+u = i % 10;
+if (i < 0)
+u = -u;
+_putchar(u + '0');
+}
