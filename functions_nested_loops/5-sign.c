dc1bd25132b2925a2293cabf417bb8831a1987bb
ariel <ariel2trabajo@gmail.com>
2024-06-17 14:36:40 +0000

    assa

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -16,16 +16,16 @@ _putchar('+');
 return (1);
 }
 
-else if (n < 48)
+else if (n == 48)
 {
-_putchar('-');
-return (-1);
+_putchar('0');
+return (0);
 }
 
 else
 {
-_putchar('0');
-return (0);
+_putchar('-');
+return (-1);
 }
 
 return (0);
