a1460fa71a8b2ec3abde09031ddb60315d6e4aaa
ariel <ariel2trabajo@gmail.com>
2024-06-17 18:24:01 +0000

    aaaa

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * times_table- Entry point
+ * add- Entry point
  * Description: 'Escribe un texto'
+ * @a: a
+ * @b: b
  * Return: int
  */
 int add(int a, int b)
--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * times_table- Entry point
+ * print_to_98- Entry point
  * Description: 'Escribe un texto'
+ * @n: aa
  * Return: int
  */
 void print_to_98(int n)
