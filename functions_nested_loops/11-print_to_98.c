b6b63f83fa1750ca174fa3f362b10f6824cf4f84
ariel <ariel2trabajo@gmail.com>
2024-06-17 18:33:57 +0000

    kkkk

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,20 @@
  */
 void print_to_98(int n)
 {
-for (; n <= 98; n++)
+if(n < 98)
 {
-printf("%d", n);
-if (n != 98)
-printf(", ");
+for (; n < 98; n++)
+{
+printf("%d, ", n);
 }
+}
+else if(n > 98)
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+printf("98")
 _putchar('\n');
 }
