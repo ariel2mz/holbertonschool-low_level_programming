632c9c0b177b922bdcb311f8486e56f0f018eb89
ariel <ariel2trabajo@gmail.com>
2024-06-17 13:19:35 +0000

    sss

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void){
+char abecedario;
+
+for (abecedario = 'a'; abecedario <= 'z'; abecedario++)
+_putchar(abecedario);
+_putchar('\n');
+return (0);
+}
