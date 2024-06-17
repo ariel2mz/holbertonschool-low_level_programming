5a4dfd875b28ca152847419e2b09fe4fbe0c7c2f
ariel <ariel2trabajo@gmail.com>
2024-06-17 13:31:44 +0000

    aaa

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+print_alphabet();
+}
