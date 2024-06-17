2b45ad721206b54bbc6c328c07a3c7ffe7511919
ariel <ariel2trabajo@gmail.com>
2024-06-17 12:48:13 +0000

    a

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+}
