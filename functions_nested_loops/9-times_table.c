99bfb73bffc433ac1c5db625f4dcd0d28648ea69
ariel <ariel2trabajo@gmail.com>
2024-06-17 17:20:15 +0000

    aaaa

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table- Entry point
+ *
+ * Description: 'Escribe un texto'
+ * @i: caracter a revisar
+ * Return: int
+ */
+void times_table(void){
+    int i;
+    int j;
+    int k;
+
+    for(i = 0; i <= 9; i++){
+        for (j = 0; j <= 9; j++){
+            k = i * j;
+            if(k >= 10){
+            _putchar((k / 10) + '0');
+            _putchar((k % 10) + '0');
+            }
+            else
+            _putchar(' ');
+            _putchar(k +'0');
+        
+        }
+        
+            }
+}
