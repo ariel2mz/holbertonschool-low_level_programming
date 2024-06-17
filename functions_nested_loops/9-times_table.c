790f87f3ac8be56695b0317f970034c42e54e2ef
ariel <ariel2trabajo@gmail.com>
2024-06-17 17:55:36 +0000

    aaaa

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,55 +1,35 @@
 #include "main.h"
 /**
  * times_table- Entry point
- *
  * Description: 'Escribe un texto'
- * @i: caracter a revisar
  * Return: int
  */
-void times_table(void){
-    int i;
-    int j;
-    int k;
-    int r;
-    int d;
+void times_table(void)
+{
+int i, j, k, r, d;
 
-    for(i = 0; i <= 9; i++){
-        for (j = 0; j <= 9; j++){
-            k = i * j;
-            if(k > 9 && j != 9){
-                d = k/10;
-                r = k%10;
-            _putchar(' ');
-            _putchar(d + '0');
-            _putchar(r + '0');
-            _putchar(',');
-            }
-            else if(k <= 9 && j != 0 && j != 9){
-            _putchar(' ');
-            _putchar(' ');
-            _putchar(k +'0');
-            _putchar(',');
-            }
-            else if(j == 0){
-                _putchar('0');
-                _putchar(',');
-            }
-            else{
-                if (k > 9){
-                    d = k/10;
-                r = k%10;
-            _putchar(' ');
-            _putchar(d + '0');
-            _putchar(r + '0');
-                }
-                else{
-                    _putchar(' ');
-            _putchar(' ');
-            _putchar(k +'0');
-                }
-            }
-        }
-        _putchar('\n');
-        
-            }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (j != 0)
+_putchar(' ');
+k = i * j;
+d = k / 10;
+r = k % 10;
+if (k > 9)
+{
+_putchar(d + '0');
+_putchar(r + '0');
+}
+else if (k <= 9)
+{
+_putchar(' ');
+_putchar(k + '0');
+}
+if (j != 9)
+_putchar(',');
+}
+_putchar('\n');
+}
 }
