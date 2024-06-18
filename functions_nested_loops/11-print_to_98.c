2ec2f19806ddfeda3e87ade386432396ff0b313a
root <ariel2morza@gmail.com>
2024-06-18 13:50:09 +0000

    11 arreglado

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,17 +3,17 @@
 /**
  * print_to_98- Entry point
  * Description: 'Escribe un texto'
- * @n:
+ * @n: hhhh
  * Return: int
  */
 void print_to_98(int n)
 {
-	if(n <= 98)
+	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			if ( n != 98 )
+			if (n != 98)
 				printf(", ");
 		}
 	}
@@ -22,7 +22,7 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-			if ( n != 98 )
+			if (n != 98)
 				printf(", ");
 		}
 	}
