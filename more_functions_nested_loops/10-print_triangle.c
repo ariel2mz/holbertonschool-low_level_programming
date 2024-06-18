dd30c88ae91baf804cab0fce889391562ab1c195
ariel <ariel2morza@gmail.com>
2024-06-18 20:13:09 +0000

    jh

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,18 @@
  * @j: aaa
  * Return: void
  */
-void print_triangle(int size){
+void print_triangle(int size)
+{
 int i;
 int j;
 
-if(size <= 0)
+if (size <= 0)
 _putchar('\n');
 for (i = 1; i <= size; i++)
 {
 for (j = size - 1; j >= 0; j--)
 {
-if(j < i)
+if (j < i)
 _putchar('#');
 else
 _putchar(' ');
