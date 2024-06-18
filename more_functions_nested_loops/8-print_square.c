f5a44454891bd4edc714adb6f9643f49bf8d70af
ariel <ariel2morza@gmail.com>
2024-06-18 17:48:37 +0000

    k

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_square - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * @c: caracter a revisar
+ * Return: void
+ */
+int print_square(int c)
+{
+int i;
+int j;
+
+for (i = 0; i <= c; i++)
+{
+for (j = 0; i <= c; i++)
+_putchar('#');
+_putchar('\n');
+}
+}
