54cd9813c5db5ac77439cea4aca48c1172706c42
ariel <ariel2morza@gmail.com>
2024-06-18 15:46:28 +0000

    lkl

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_line - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void print_line(int n)
+{
+int i
+
+for (i = 0; i <= n; i++)
+{
+_putchar('_');   
+}
+_putchar('\n');
+}
