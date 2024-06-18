0bd716bcb6ec69b174e66a8b49e13c89d36f7ead
ariel <ariel2morza@gmail.com>
2024-06-18 14:44:01 +0000

    kjhg

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _isdigit - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * @c: caracter a revisar
+ * Return: void
+ */
+int _isdigit(int c)
+{
+int i;
+
+for (i = '0'; r <= '9'; r++)
+{
+if (i == c)
+{
+return (1);
+}
+}
+return (0);
+}
