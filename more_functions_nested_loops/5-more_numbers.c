9c6d816e1427de60c886d59e5288a86e2043c482
ariel <ariel2morza@gmail.com>
2024-06-18 15:20:13 +0000

    lll

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i;
+for (j = 0; j <= 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 10)
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+}
+}
