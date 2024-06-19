0eafdcd352dc32bc4c14e8a7900f6f965ad02312
ariel <ariel2morza@gmail.com>
2024-06-19 13:48:13 +0000

    0

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * swap_int - Entry point
+ *
+ * @a: aaa
+ * @b: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void swap_int(int *a, int *b);
+{
+int *aux = *a;
+ 
+*a = *b;
+*b = *aux;
+}
