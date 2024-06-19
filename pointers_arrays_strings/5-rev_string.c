a327b3194e10eed3177c285d0f4701545d1ccbc6
ariel <ariel2morza@gmail.com>
2024-06-19 16:14:32 +0000

    0

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,19 +11,31 @@
  */
 void rev_string(char *s)
 {
-  char pletra;
+  char pletra,
 char *aux2;
 char letra;
+const char *p;
+ int largo;
+ int i;
  
- pletra = *s;
+if (s == NULL)
+return (0);
+p = s;
+while (*p != '\0')
+p++;
+largo = p - s;
+ 
 aux2 = s;
 while (*aux2 != '\0')
 {
 aux2++;
 }
- while (*aux2 != pletra)
+
+ aux2--;
+ for (i = 0; i <= (largo/2); i++)
 {
-letra = *aux2;  
+letra = *aux2;
+ *aux2 = *s;
 *s = letra;
 
  aux2--;
