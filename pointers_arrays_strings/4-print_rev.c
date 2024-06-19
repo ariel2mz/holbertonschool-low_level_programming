ce2f3f15b0449415f64725875337af705435b40a
ariel <ariel2morza@gmail.com>
2024-06-19 14:34:21 +0000

    0

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * print_rev - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void _puts(char *s)
+{
+char *aux;
+
+aux = s;
+while (*s != '\0')
+{
+s++;
+}
+while (*s != *aux)
+{
+s--;
+_putchar(*str);
+}
+_putchar('\n');
+}
