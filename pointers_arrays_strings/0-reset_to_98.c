08519754e0fac569187e024856cee9922b4fd712
ariel <ariel2morza@gmail.com>
2024-06-19 13:36:58 +0000

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+int *n = 98;
+}
