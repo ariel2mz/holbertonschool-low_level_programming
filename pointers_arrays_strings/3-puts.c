383cd50b4b2ffea0044d1395395efbc0b0b5f742
ariel <ariel2morza@gmail.com>
2024-06-19 14:20:48 +0000

    0

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _puts - Entry point
+ *
+ * @str: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void _puts(char *str)
+{
+while (str != '\0')
+{
+_putchar(str);
+str++;
+}
+}
+
