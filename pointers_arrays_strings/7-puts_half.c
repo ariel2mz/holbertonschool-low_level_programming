ce3bc6e9c8f9e0463d75d4be75a208f599d75423
ariel <ariel2morza@gmail.com>
2024-06-20 15:49:30 +0000

    kk

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * puts_half - Entry point
+ *
+ * @str: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int l;
+
+l = _strlen(*str);
+if ((l % 2) != 0)
+l--
+while (*str != '\0')
+{
+if (i > (l / 2))
+_putchar(*str);
+i++;
+str++;
+}
+_putchar('\n');
+}
