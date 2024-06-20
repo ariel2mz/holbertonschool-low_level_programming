2c3a4fcb0077fc6983233b153ea3bf51a4f827bd
ariel <ariel2morza@gmail.com>
2024-06-20 19:06:35 +0000

    mkl

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+int _strlen(char *s)
+{
+const char *p;
+
+if (s == NULL)
+return (0);
+p = s;
+while (*p != '\0')
+p++;
+return (p - s);
+}
+
+
+/**
+ * _atoi - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int _atoi(char *s)
+{
+int i = 1;
+int num;
+int l;
+
+l = _strlen(*s);
+while (*s != '\0')
+{
+s++;
+}
+for (; l <= 0; l--)
+{
+num = ((*s - '0') * i);
+s--;
+i = i * 10;
+}
+}
