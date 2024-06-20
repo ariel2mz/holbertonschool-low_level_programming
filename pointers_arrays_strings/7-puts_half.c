087ad5f87b2e5175524f830c4210773b15b49737
ariel <ariel2morza@gmail.com>
2024-06-20 16:07:43 +0000

    kk

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,29 +9,33 @@
  * Description: 'Escribe un texto'
  * Return: void
  */
+
+
+int _strlen(char *s)
+{
+const char *p;
+
+if (s == NULL)
+return (0);
+p = s;
+while (*p != '\0')
+p++;
+return (p - s);
+}
+
+
 void puts_half(char *str)
 {
 int i = 0;
 int l;
-const char *p;
 
-if (str == NULL)
-p = str;
-while (*p != '\0')
-p++;
-l = p - str;
-
-
-
-
-
-
-if ((l % 2) != 1)
+l = _strlen(str);
+if ((l % 2) != 0)
 l--;
 while (*str != '\0')
 {
 if (i > (l / 2))
-_putchar(l + '0');
+_putchar(*str);
 i++;
 str++;
 }
