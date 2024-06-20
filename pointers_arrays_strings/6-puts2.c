5653b81ad74d0304d4f5ebb814fe5dff131767d6
ariel <ariel2morza@gmail.com>
2024-06-20 15:37:09 +0000

    k

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * puts2 - Entry point
+ *
+ * @str: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+void puts2(char *str)
+{
+
+int i;
+
+while (*str != '\0')
+{
+i++;
+if((i / 2) == 1)
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
