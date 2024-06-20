02ba2521f4622b326b33e9b64ab31096d4464dc7
ariel <ariel2morza@gmail.com>
2024-06-20 18:50:00 +0000

    kjn

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int _strlen(char *s)
+{
+const char *p;
+
+if (s == NULL)
+return (0);
+p = s;
+while (*p != '\0')
+p++;
+return (p - s);
+}
+
+
+/**
+ * _strcpy - Entry point
+ *
+ * @dest: aaa
+ * @src: aa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+char *_strcpy(char *dest, char *src)
+{
+int l;
+
+l = _strlen(dest);
+for (i = 0; i <= l;i++)
+dest[i] = src[i];    
+return *dest;
+}
