1a80931666f32d10ddd1ca855b8758b8d0f2c3cb
ariel <ariel2morza@gmail.com>
2024-06-20 16:18:53 +0000

    asd

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * print_array - Entry point
+ *
+ * @a: aaa
+ * @n: aa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i<n; i++)
+a++;
+printf("%d, ", *a);
+}
