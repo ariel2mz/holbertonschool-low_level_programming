0ca7e038673ef50fb8eec1ccff473a6a31a7e0bc
ariel <ariel2morza@gmail.com>
2024-06-24 13:15:21 +0000

    ksd

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,26 +18,24 @@ p++;
 l = p - s;
 for (i = 0; i <= l; i++)
 {
-
-    if (*s == '-')
-    {
-        neg = -neg;
-    }
-    if (*s >= 48 && *s <= 58){
-        if(num != 0)
-            num = num * 10;
-
-        if (neg == -1)
-        num = num - (*s - '0');
-        else
-        num = num + (*s - '0');
-        s++;
-        if (*s == ' ')
-            break;
-        s--;
-    }
-    s++;
-    
+if (*s == '-')
+{
+neg = -neg;
+}
+if (*s >= 48 && *s <= 58)
+{
+if (num != 0)
+num = num * 10;
+if (neg == -1)
+num = num - (*s - '0');
+else
+num = num + (*s - '0');
+s++;
+if (*s == ' ')
+break;
+s--;
+}
+s++;    
 }
 return (num);
 }
