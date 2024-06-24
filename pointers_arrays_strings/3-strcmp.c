48022699b330115f80fd43d10e42505a7551eb47
ariel <ariel2morza@gmail.com>
2024-06-24 15:42:19 +0000

    asd

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strncpy - Entry point
+ *
+ * @dest: aaa
+ * @src: aaa
+ * @n: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int v1 = 0;
+int v2 = 0;
+
+while (*s1 != '\0')
+{
+v1 = v1 + *s1;
+s1++
+}
+while (*s2 != '\0')
+{
+v2 = v2 + *s2;
+s2++;
+}
+return (s1 - s2);
+}
