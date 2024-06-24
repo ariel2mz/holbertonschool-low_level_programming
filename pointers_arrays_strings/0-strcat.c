ababd80e447457d4c609fa5722499cf3d5f232b8
ariel <ariel2morza@gmail.com>
2024-06-24 13:47:45 +0000

    asdsa

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+while (*dest != '\0')
+    dest++;
+while (*src != '\0')
+{
+    *dest = *src;
+    src++;
+    dest++;
+}
+
+return (dest);
+}
