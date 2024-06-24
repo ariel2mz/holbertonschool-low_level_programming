4d904e6d75fe214117314c085173750df98c04ec
ariel <ariel2morza@gmail.com>
2024-06-24 18:48:34 +0000

    ads

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@
  */
 char *leet(char *s)
 {
-char a[] = {a, A, e, E, o, O, t, T, l, L};
+char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 char b[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 char *c = s;
 int i;
