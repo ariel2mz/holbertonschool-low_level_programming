6d594de3593173c4e0a33fa43cf24cdd38764211
ariel <ariel2morza@gmail.com>
2024-06-24 12:53:03 +0000

    as

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@
 int _atoi(char *s)
 {
 const char *p;
-int l, i;
+int l, i, num = 0;
 bool neg;
 p = s;
 while (*p != '\0')
@@ -18,16 +18,16 @@ p++;
 l = p - s;
 for (i = 0; i < l; i++)
 {
-    if (s == '-')
+    if (s* == '-')
     {
      s++
-     if (s <= 48 && s >= 58)
+     if (s* <= 48 && s* >= 58)
      {
         neg = TRUE;
      }
     s--
     }
-    if (s <= 48 && s >= 58){
+    if (s* <= 48 && s* >= 58){
         if(num != 0)
             num = num * 10;
         num = num + (s - '0');
