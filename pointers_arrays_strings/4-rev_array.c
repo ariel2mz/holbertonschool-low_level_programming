26128dce16a02fd1306e9c6cec06b2b59e1b24c2
ariel <ariel2morza@gmail.com>
2024-06-24 15:57:58 +0000

    as

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * reverse_array - Entry point
+ *
+ * @a: aaa
+ * @n: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+void reverse_array(int *a, int n)
+{
+int i = 0, j = n - 1;
+while (j > i)
+{
+a[i] = a[j];
+i--
+j--
+}
+}
