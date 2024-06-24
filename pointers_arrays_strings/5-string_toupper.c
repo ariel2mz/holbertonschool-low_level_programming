19268b2e5a13bd4a98ff810a27eb22ad01092f57
ariel <ariel2morza@gmail.com>
2024-06-24 16:10:47 +0000

    asd

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * string_toupper - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+char *string_toupper(char *s)
+{
+char *a = s;
+while(*a != '\0')
+{
+if(*a >= 65 && *a <= 90)
+*a = *a + 32;
+}
+
+}
