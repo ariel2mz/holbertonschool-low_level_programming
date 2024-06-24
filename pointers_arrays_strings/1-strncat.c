17d185b6fa0f05c87d070cb3ad99be50fea47819
ariel <ariel2morza@gmail.com>
2024-06-24 14:33:41 +0000

    aa

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strncat - Entry point
+ *
+ * @dest: aaa
+ * @src: aaa
+ * @n: aaa
+ * Description: 'Escribe un texto'
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *a = dest;
+int i;
+
+while (*a != '\0')
+{
+a++;
+}
+for (i = 0; i <= n; i++)
+{
+*a = *src;
+src++;
+a++;
+}
+return (dest);
+}
