41969d849429e1fb3a9442997159147cafc20bfc
ariel <ariel2morza@gmail.com>
2024-06-25 18:32:03 +0000

    a

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,14 @@
  */
 char *_strchr(char *s, char c)
 {
-  while (*s != c && *s != '\0' )
-  {
-  s++;
-  }
-  if (c == '\0')
-    return (s);
-  else if (*s == '\0')
-    return (NULL);
-  else
-    return (s);
+while (*s != c && *s != '\0')
+{
+s++;
+}
+if (c == '\0')
+return (s);
+else if (*s == '\0')
+return (NULL);
+else
+return (s);
 }
