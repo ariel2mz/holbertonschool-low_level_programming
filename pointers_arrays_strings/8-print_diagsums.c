9a1ab8f218feb237dd0b142fc7088b38aacf2870
ariel <ariel2morza@gmail.com>
2024-06-25 22:07:57 +0000

    a

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,16 +12,23 @@
 void print_diagsums(int *a, int size)
 {
   int i, j;
+  int iaux, jaux;
   int diagonal1 = 0, diagonal2 = 0;
-  for (i = 0; i <= size; i++)
-    for (j = 0; j <= size; j++)
+  for (i = 0; i < size; i++)
+    for (j = 0; j < size; j++)
       {
 	if (j == i)
 	  {
-	    diagonal1 = diagonal1 + a[j][i];
+	    jaux = j * size;
+	    iaux = i + jaux;
+	    diagonal1 = diagonal1 + a[iaux];
 	  }
 	if ((j + i - 1) == size)
+	  {
+	    jaux = j * size;
+            iaux = i + jaux;
 	  diagonal2 = diagonal2 + a[i][j];
+	]
       }
 }
 
