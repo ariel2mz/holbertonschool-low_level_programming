ccee11e787d656a74acf0c03de66bda22cef26b1
ariel <ariel2morza@gmail.com>
2024-06-25 19:12:54 +0000

    a

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+/* more headers goes there */
+
+/**
+ * _strspn - Entry point
+ *
+ * @s: aaa
+ * @accept: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int contador = 0;
+char *x;
+
+while (*s != '\0')
+{
+x = accept;
+while (*x != '\0' && *x != *s)
+{
+x++;
+}
+if (*x == *s)
+return (s);
+s++;
+}
+return (NULL);
+}
