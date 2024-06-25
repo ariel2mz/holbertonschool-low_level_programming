951996710f68630d763fe2db43942d0e78ba7278
ariel <ariel2morza@gmail.com>
2024-06-25 19:32:13 +0000

    a

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strspn - Entry point
+ *
+ * @s: aaa
+ * @accept: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+  while(*haystack != '/0')
+    {
+      if (*haystack == needle[0])
+	{
+	  aux = haystack;
+	  aux2 = needle;
+	  while((*aux == *aux2) && (*aux2 != '\0'))
+	    {
+	      aux++;
+	      aux2++;
+	    }
+	  if (*aux2 == '\0')
+	    return (*haystack);
+	    
+	}
+    }
+  return('\0');
+}
