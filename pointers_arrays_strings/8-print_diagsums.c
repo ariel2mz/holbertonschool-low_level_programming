1233b9fdac8091888280a8d118cd1ff6a29d2c10
ariel <ariel2morza@gmail.com>
2024-06-25 22:24:11 +0000

    a

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,35 +3,35 @@
 /* more headers goes there */
 
 /**
- * cap_string - Entry point
+ * print_diagsums - Entry point
  *
- * @s: aaa
+ * @a: aaa
+ * @size: aa
  * Description: 'Escribe un texto'
  * Return: int
  */
 void print_diagsums(int *a, int size)
 {
-  int i, j;
-  int iaux, jaux;
-  int diagonal1 = 0, diagonal2 = 0;
-  for (i = 0; i < size; i++)
-    for (j = 0; j < size; j++)
-      {
-	if (j == i)
-	  {
-	    jaux = j * size;
-	    iaux = i + jaux;
-	    diagonal1 = diagonal1 + a[iaux];
-	  
-	  }
-	if ((j + i + 1) == size)
-	  {
-	    jaux = j * size;
-            iaux = i + jaux;
-	  diagonal2 = diagonal2 + a[iaux];
-	  }
-      }
-  printf("%d, ",diagonal1);
-   printf("%d\n", diagonal2);
+int i, j;
+int iaux, jaux;
+int diagonal1 = 0, diagonal2 = 0;
+for (i = 0; i < size; i++)
+for (j = 0; j < size; j++)
+{
+if (j == i)
+{
+jaux = j * size;
+iaux = i + jaux;
+diagonal1 = diagonal1 + a[iaux];	  
+}
+if ((j + i + 1) == size)
+{
+jaux = j * size;
+iaux = i + jaux;
+diagonal2 = diagonal2 + a[iaux];
+}
+}
+printf("%d, ",diagonal1);
+printf("%d\n", diagonal2);
 }
 
