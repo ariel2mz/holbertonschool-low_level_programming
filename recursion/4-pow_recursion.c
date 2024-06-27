e516da136a6fdd3f6fff9bdef711c092f9ed4b9f
ariel <ariel2morza@gmail.com>
2024-06-27 14:15:53 +0000

    sad

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strlen_recursion - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0){
+    return (-1);
+}
+    return (1);
+}
+return (_pow_recursion(x * x, y - 1));
+}
