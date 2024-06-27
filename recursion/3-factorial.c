c7efceb11ef6ecd9d3275d0b181be8f70a82bafb
ariel <ariel2morza@gmail.com>
2024-06-27 13:43:02 +0000

    sada

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * _strlen_recursion - Entry point
+ *
+ * @s: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int factorial(int n)
+{
+if (n < 0){
+    return (-1);
+}
+if (n = 0){
+    return (1);
+}
+return (factorial(n - 1) * n);
+
+}
