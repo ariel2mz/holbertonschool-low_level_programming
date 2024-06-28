514895a17fcb16fdd1cdad222a7c9c2fbcf1f9f3
ariel <ariel2morza@gmail.com>
2024-06-28 16:02:34 +0000

    a

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,17 +10,17 @@
  * Description: 'Escribe un texto'
  * Return: int
  */
-int aux(int n, int g)
+int aux2(int n, int g)
 {
   if (g == 0)
     return (1);
   if (n % g == 0)
     return (0);
   else
-    aux(n, g - 1);
+   return  aux2(n, g - 1);
 }
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
   int a;
-a = aux(n, n - 1);
-    return a; 
+a = aux2(n, n - 1);
+return (a); 
 }
