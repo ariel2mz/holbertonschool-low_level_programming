3603d30738dab538e003e02240bc11656faf98b5
ariel <ariel2morza@gmail.com>
2024-06-28 16:29:58 +0000

    a

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,25 +3,34 @@
 /* more headers goes there */
 
 /**
- * _pow_recursion - Entry point
+ * aux2 - Entry point
  *
- * @x: aaa
- * @y: aaa
+ * @n: aaa
+ * @g: aaa
  * Description: 'Escribe un texto'
  * Return: int
  */
 int aux2(int n, int g)
 {
-  if (g == 0)
-    return (0);
-  if (n % g == 0)
-    return (1);
-  else
-   return  aux2(n, g - 1);
+if (g == 0)
+return (0);
+if (n % g == 0)
+return (1);
+else
+return  aux2(n, g - 1);
 }
+/**                                                                                                                                                                                                                                       
+ * _is_prime_number - aa                                                                                                                                                                                                                   
+ * @n: aaa                                                                                                                                                                                                                               
+ * Description: 'Escribe un texto'                                                                                                                                                                                                       
+ * Return: int                                                                                                                                                                                                                           
+ */
 int is_prime_number(int n)
 {
-  int a;
+int a;
+ 
+if (n < 0)
+n = -n;
 a = aux2(n, n - 1);
 return (a); 
 }
