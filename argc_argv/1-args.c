cf5a12114fdc450d8a67c3fb5ea6b046bf5bcffc
ariel <ariel2morza@gmail.com>
2024-06-29 23:33:41 +0000

    asd

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * @argc: aaa
+ * @argv: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+if (argv[0] != '0')
+printf("%d\n", argc);
+return (0);
+}
