2c4baa1b7914a7e170f5940f4b3254d14f107756
ariel <ariel2morza@gmail.com>
2024-06-29 23:27:54 +0000

    asd

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
  
-        printf("%s ", argv[argc]);        /* Printing the string */
+        printf("%s ", argv[argc - 1]);        /* Printing the string */
 
     
 
