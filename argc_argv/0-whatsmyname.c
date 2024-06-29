8d0717b1079de1210b05beb4b3ca59f4e5769111
ariel <ariel2morza@gmail.com>
2024-06-29 23:16:33 +0000

    asd

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * factorial - Entry point
+ *
+ * @n: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+main(int argc, char *argv[])
+{
+printf("%s",argv);
+}
