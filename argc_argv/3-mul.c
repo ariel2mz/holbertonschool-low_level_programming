172b12d963becee1474910485f96a658e2be9386
ariel <ariel2morza@gmail.com>
2024-06-30 00:55:15 +0000

    ads

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * @argc: aaa
+ * @argv: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+
+
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+return (1);
+return(argv[1] * argv[2])
+}
