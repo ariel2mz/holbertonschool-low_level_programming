64c11998024ece79a6ce7ee4b0f4309972bfdd9c
ariel <ariel2morza@gmail.com>
2024-06-30 20:52:41 +0000

    h0

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -19,8 +19,8 @@ int result, a, b;
 
 if (argc != 3)
 return (1);
-a = atoi(*argv[1]);
-b = atoi(*argv[2]);
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 result = a * b;
 printf("%d",result);
 return(0);
