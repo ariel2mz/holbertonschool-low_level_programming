4a58601520c8214af1f858df6d0adecca086f611
ariel <ariel2morza@gmail.com>
2024-06-30 01:13:37 +0000

    asda

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,18 +13,13 @@
 
 int main(int argc, char *argv[])
 {
-int result;
-int a;
-int b;
-
+int result, a, b;
 
 if (argc != 3)
 return (1);
-printf("%d\n", *argv[1]);
-*argv[1]++;
-printf("%d\n", *argv[1]);
-
-result = ((*argv[1] - '0')*(*argv[2] - '0'));
-
-return(result);
+a = atoi(*argv[1]);
+b = atoi(*argv[2]);
+result = a * b;
+printf("%d",result);
+return(0);
 }
