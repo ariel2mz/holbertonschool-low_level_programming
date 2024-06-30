d111c771cd9ceb62e66bfb046a72519be3adcc4a
ariel <ariel2morza@gmail.com>
2024-06-30 21:02:36 +0000

    h0

---
+++ argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - asdsad
+* @argc: fsadas
+* @argv: aaa
+*
+* Description: asdasdsadasda
+*
+* Return: 0 on succes
+*/
+int main(int argc, char *argv[])
+{
