4ebbcf8ebef20a5188e77f359d0323a2abde8fe1
ariel2mz <ariel2trabajo@gmail.com>
2024-06-30 20:09:08 -0100

    444

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,3 +12,32 @@
 */
 int main(int argc, char *argv[])
 {
+int i, j;
+int result;
+  
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+else
+{
+result = 0;
+for (i = 1; i < argc; i++)
+{
+j = 0;
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+result = result + atoi(argv[i]);
+}
+}
+printf("%d", result);
+printf("\n");
+return (0);
+}
+}
