7a0d14db395be7ab383bada7f0187ff5b352650b
ariel <ariel2morza@gmail.com>
2024-06-30 01:05:20 +0000

    asd

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,10 +14,17 @@
 int main(int argc, char *argv[])
 {
 int result;
+int a;
+int b;
+
 
 if (argc != 3)
 return (1);
+printf("%d\n", *argv[1]);
+*argv[1]++;
+printf("%d\n", *argv[1]);
+
 result = ((*argv[1] - '0')*(*argv[2] - '0'));
-printf("%d\n", result);
+
 return(result);
 }
