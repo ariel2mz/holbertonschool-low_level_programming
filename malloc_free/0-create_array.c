dab8a51156a206f2f4337bb6c0f0a50bcc12c978
ariel <ariel2morza@gmail.com>
2024-07-01 16:01:21 +0000

    sadsa

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * @argc: aaa
+ * @argv: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+char *create_array(unsigned int size, char c)
+{
+char *result;
+
+ar = malloc(size);
+for (i = 0; i < size; i++)
+result[i] = c;
+return *result;
+}
