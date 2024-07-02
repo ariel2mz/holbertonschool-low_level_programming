27323a75f05cc27010b4dd4825a95c7d477ca743
ariel <ariel2morza@gmail.com>
2024-07-02 14:27:27 +0000

    a

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,28 +13,29 @@
  */
 char *_strdup(char *str)
 {
-  unsigned int i = 0;
- unsigned int x;
- char *result;
+unsigned int i = 0;
+unsigned int x;
+char *result;
 
- if (str == NULL)
-   return NULL;
- if (*str == '\0')
-   {
-     result = malloc(1);
-   return(result);
-   } 
- while (*str != '\0'){
-   i++;
-     str++;
- }
- for (x = 0; x < i; x++)
-   str--;
- result = malloc((i + 1) * sizeof(char));
- if (result == NULL)
-   return NULL;
- for (x = 0; x < i; x++){
-  result[x] = str[x];
- }
+if (str == NULL)
+return (NULL);
+if (*str == '\0')
+{
+result = malloc(1);
+return (result);
+} 
+while (*str != '\0'){
+i++;
+str++;
+}
+for (x = 0; x < i; x++)
+str--;
+result = malloc((i + 1) * sizeof(char));
+if (result == NULL)
+return (NULL);
+for (x = 0; x < i; x++)
+result[x] = str[x];
 return (result);
 }
+
+
