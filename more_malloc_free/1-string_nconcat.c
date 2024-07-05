670d434d051e8ca911c512028078a020a5b9ed86
ariel <ariel2morza@gmail.com>
2024-07-05 17:30:06 +0000

    sad

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     char *result;
     
     
-   if (s1 == NULL)
-    s1 = "";
-    if (s2 == NULL)
-    s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
     while (*s1 != '\0')
         {
         s1len++;
