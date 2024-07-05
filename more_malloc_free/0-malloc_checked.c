8c762910b4c381f096366c454c430462a2992e8f
ariel <ariel2morza@gmail.com>
2024-07-05 13:19:02 +0000

    kj

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* more headers goes there */
+
+/**
+ * free_grid - Entry point
+ *
+ * @grid: aaa
+ * @height: aaax
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+void *malloc_checked(unsigned int b)
+{
+int *p;
+
+p = malloc(b);
+if (p == NULL)
+return (98);
+return (p);
+}
