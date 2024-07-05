7e68b642a9fdf345d10fa4d73371cfebd3ef0ae7
ariel <ariel2morza@gmail.com>
2024-07-05 17:47:15 +0000

    sadsa

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,49 +4,49 @@
 /* more headers goes there */
 
 /**
- * malloc_checked - Entry point
+ * string_nconcat - Entry point
  *
- * @b: bsada
+ * @s1: bsada
+ * @s2: asdasdasds
+ * @n: awpwap
  * Description: 'Escribe un texto'
  * Return: int
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int s1len = 0, i, s2len = 0, j;
-    char *result;
-    
-    
+unsigned int s1len = 0, i, s2len = 0, j;
+char *result;
+        
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-    while (*s1 != '\0')
-        {
-        s1len++;
-        s1++;
-        }
-    for (i = 0; i < s1len; i++)
-    s1--;
-    while (*s2 != '\0')
-        {
-        s2len++;
-        s2++;
-        }
-    for (i = 0; i < s2len; i++)
-    s2--;
-    if (n > s2len)
-    n = s2len;
-    result = malloc(s1len + n + 1);
-    if (result == NULL)
-    return (NULL);
-
-    for (i = 0; i < s1len; i++)
-    result[i] = s1[i];
-    for (j = 0; j < n; j++)
-    {
-    result[i] = s2[j];
-    i++;
-    }
-    result[i] = '\0';
-    return (result);
+while (*s1 != '\0')
+{
+s1len++;
+s1++;
+}
+for (i = 0; i < s1len; i++)
+s1--;
+while (*s2 != '\0')
+{
+s2len++;
+s2++;
+}
+for (i = 0; i < s2len; i++)
+s2--;
+if (n > s2len)
+n = s2len;
+result = malloc(s1len + n + 1);
+if (result == NULL)
+return (NULL);
+for (i = 0; i < s1len; i++)
+result[i] = s1[i];
+for (j = 0; j < n; j++)
+{
+result[i] = s2[j];
+i++;
+}
+result[i] = '\0';
+return (result);
 }
