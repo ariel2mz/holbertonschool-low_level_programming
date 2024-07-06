3d5dd7c5f969d4fed0f88e51b4bad3e34c56fad5
ariel <ariel2morza@gmail.com>
2024-07-06 19:28:05 +0000

    sada

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* more headers goes there */
+
+/**
+ * string_nconcat - Entry point
+ *
+ * @s1: bsada
+ * @s2: asdasdasds
+ * @n: awpwap
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+ void *_calloc(unsigned int nmemb, unsigned int size)
+ {
+void *result;
+int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+result = malloc(nmemb * size);
+if (result == NULL)
+return (NULL);
+for (i = 0; i < nmemb; i++)
+result[i] = 0;
+ }
