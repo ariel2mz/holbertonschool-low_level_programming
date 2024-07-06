afb673a9ffb7f7e4db5283d074b867fb442de8e2
ariel <ariel2morza@gmail.com>
2024-07-06 20:33:36 +0000

    asda

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* more headers goes there */
+
+/**
+ * init_dog - Entry point
+ *
+ * @d: bsada
+ * @name: asdasdasds
+ * @age: adsadas
+ * @owner: adsadas
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+ d->name = name;
+ d->age = age;
+ d->owner = owner;
+}
