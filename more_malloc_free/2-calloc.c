4cb654eb92e361d96f4dcd15d84fe99fed65c99f
ariel <ariel2morza@gmail.com>
2024-07-06 19:33:37 +0000

    asda

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,12 +14,16 @@
  */
  void *_calloc(unsigned int nmemb, unsigned int size)
  {
-void *result;
+int *result;
+unsigned int i;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
 result = malloc(nmemb * size);
 if (result == NULL)
 return (NULL);
+for (i = 0; i < nmemb; i++)
+result[i] = 0;
+
 return (result);
  }
