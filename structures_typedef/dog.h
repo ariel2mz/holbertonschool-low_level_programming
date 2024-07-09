c57bd8ebbceaec491be1287865eb52eebabfeaf7
ariel <ariel2morza@gmail.com>
2024-07-09 14:13:22 +0000

    asdsa

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,8 +15,8 @@ typedef struct dog
 char *name;
 float age;
 char *owner;
-} dog;
-typedef struct dog_t; 
+} dog, dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
