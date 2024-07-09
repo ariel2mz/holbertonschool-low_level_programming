ed7735f61f9b101ba29e3913a5093f13c5300de9
ariel <ariel2morza@gmail.com>
2024-07-09 14:06:44 +0000

    sada

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,12 +19,9 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-if (d->age != NULL)
 printf("Age: %d\n", d->age);
-else
-printf("Age: (nil)\n");
 if (d->owner != NULL)
-printf("Owner: %d\n");
+printf("Owner: %s\n", d->owner);
 else
 printf("Owner: (nil)\n");
 }
