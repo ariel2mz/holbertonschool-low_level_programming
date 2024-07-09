73637e907f3e6380c1372336f87ed2aa4b22b41a
ariel <ariel2morza@gmail.com>
2024-07-09 14:49:56 +0000

    sadas

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,6 +6,42 @@
 /* more headers goes there */
 
 /**
+ * _strncpy - Entry point
+ *
+ * @dest: aaa
+ * @src: aaa
+ * @n: aaa
+ * Description: 'Escribe un texto'
+ * Return: int
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+char *a = dest;
+int l;
+const char *p;
+
+p = src;
+while (*p != '\0')
+p++;
+l = p - src;
+
+
+for (i = 0; i < n; i++)
+{
+if (i > l)
+*a = '\0';
+else
+*a = *src;
+a++;
+src++;
+}
+return (dest);
+}
+
+/* more headers goes there */
+
+/**
  * _strlen - Entry point
  *
  * @s: aaa
