97e6fa27661cda3441e2ec9f25715d0edb895546
ariel <ariel2morza@gmail.com>
2024-07-09 14:07:19 +0000

    qweqw

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-printf("Age: %d\n", d->age);
+printf("Age: %f\n", d->age);
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
