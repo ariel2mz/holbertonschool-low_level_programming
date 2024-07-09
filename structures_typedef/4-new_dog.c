152346820526ded83aab40159113d2c20595950a
ariel <ariel2morza@gmail.com>
2024-07-09 14:46:39 +0000

    sad

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -35,7 +35,7 @@ return (p - s);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t new_dog = malloc(sizeof(dog_t));
+dog_t *new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
 new_dog->name = malloc(sizeof(_strlen(name)) + 1);
