7f549e506eefdc8ca0fd155d35e17f88333c57af
ariel <ariel2morza@gmail.com>
2024-07-10 18:16:09 +0000

    a

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include <string.h>
+
+/* more headers go here */
+
+/**
+ * print_name - Calculate the length of a string
+ *
+ * @name: The string to measure
+ * @f: aaaaa
+ * Description: Returns the length of a string
+ * Return: Length of the string
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
