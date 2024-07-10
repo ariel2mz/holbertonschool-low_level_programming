8ede50c3d8ae217a4b8f5efe7295d30cfeb719e2
ariel <ariel2morza@gmail.com>
2024-07-10 18:28:03 +0000

    a

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -6,16 +6,18 @@
 /* more headers go here */
 
 /**
- * print_name - Calculate the length of a string
+ * array_iterator - Calculate the length of a string
  *
- * @name: The string to measure
- * @f: aaaaa
+ * @array: The string to measure
+ * @size: aaaaa
+ * @action: asdsadsa
  * Description: Returns the length of a string
  * Return: Length of the string
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  size_t i;
-  for (i = 0; i < size; i++)
-    action(array[i]);
+size_t i;
+for (i = 0; i < size; i++)
+action(array[i]);
 }
+
