ebb34f7c9085f64738196a5029a3030376a0fc2c
ariel <ariel2morza@gmail.com>
2024-07-10 17:56:00 +0000

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,14 +14,13 @@
  */
 int _strlen(char *s)
 {
-    const char *p;
-
-    if (s == NULL)
-        return (0);
-    p = s;
-    while (*p != '\0')
-        p++;
-    return (p - s);
+const char *p;
+if (s == NULL)
+return (0);
+p = s;
+while (*p != '\0')
+p++;
+return (p - s);
 }
 
 /**
@@ -34,12 +33,11 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
-
-    for (i = 0; src[i] != '\0'; i++)
-        dest[i] = src[i];
-    dest[i] = '\0';
-    return (dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
 }
 
 /* more headers go here */
@@ -55,29 +53,24 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog = malloc(sizeof(dog_t));
-
-    if (new_dog == NULL)
-        return NULL;
-
-    new_dog->name = malloc(_strlen(name) + 1);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return NULL;
-    }
-
-    new_dog->owner = malloc(_strlen(owner) + 1);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return NULL;
-    }
-
-    _strcpy(new_dog->name, name);
-    _strcpy(new_dog->owner, owner);
-    new_dog->age = age;
-
-    return new_dog;
+dog_t *new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+new_dog->name = malloc(_strlen(name) + 1);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+new_dog->owner = malloc(_strlen(owner) + 1);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+_strcpy(new_dog->name, name);
+_strcpy(new_dog->owner, owner);
+new_dog->age = age;
+return new_dog;
 }
