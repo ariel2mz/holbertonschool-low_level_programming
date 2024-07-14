8467498d318cd00bf79cb9aa8d3e46bca7e82f65
ariel <ariel2morza@gmail.com>
2024-07-14 18:40:27 +0000

    sad

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - Sums all its parameters.
+ * @n: The number of arguments passed.
+ * @separator: aaaaaaa.
+ * Return: The sum of all parameters.
+ * If n is 0, returns 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+
+if (separator == NULL)
+separator = "";
+if (n == 0)
+{
+printf("\n");
+return;
+}
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+if (i + 1 != n)
+printf("%s%s", va_arg(args, char *), separator);
+else
+printf("%d\n", va_arg(args, char *));
+}
+va_end(args);
+}
