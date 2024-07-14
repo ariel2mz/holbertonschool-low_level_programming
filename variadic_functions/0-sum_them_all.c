7826ef4895886cae1f4a167439be4e1f7df87f4a
ariel <ariel2morza@gmail.com>
2024-07-14 15:00:15 +0000

    sa

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,10 +12,10 @@ unsigned int i;
 va_list args;
 int sum = 0;
 if (n == 0)
-return 0;
+return (0);
 va_start(args, n);
 for (i = 0; i < n; i++)
 sum = sum + va_arg(args, int);
 va_end(args);
-return sum;
+return (sum);
 }
