2dd25d71c9ddebbe6a8f8b9e0ef3e82423ae3c84
ariel <ariel2morza@gmail.com>
2024-07-14 15:25:59 +0000

    asd

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,6 +16,11 @@ if (n == 0)
 return;
 va_start(args, n);
 for (i = 0; i < n; i++)
+{
+if (i + 1 != n)
 printf("%d%s", va_arg(args, int), separator);
+else
+printf("%d", va_arg(args, int));
+}
 va_end(args);
 }
