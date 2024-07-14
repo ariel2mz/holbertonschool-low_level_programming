9db17eaf8a7043a7cb06d095f9162d7dab7127f7
ariel <ariel2morza@gmail.com>
2024-07-14 15:30:58 +0000

    asd

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list args;
 
-if (separator = NULL)
+if (separator == NULL)
 separator = "";
 if (n == 0)
 return;
