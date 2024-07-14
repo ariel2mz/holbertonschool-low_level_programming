c2bd3c998f932c7f7c0e17ed4e0099831ee5cab2
ariel <ariel2morza@gmail.com>
2024-07-14 15:32:16 +0000

    sad

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,7 +22,8 @@ for (i = 0; i < n; i++)
 if (i + 1 != n)
 printf("%d%s", va_arg(args, int), separator);
 else
-printf("%d\n", va_arg(args, int));
+printf("%d", va_arg(args, int));
 }
+printf("\n");
 va_end(args);
 }
