93ac49720c5986aed403ec5eaffb216523d3bfe6
ariel <ariel2morza@gmail.com>
2024-07-14 19:47:33 +0000

    asdsa

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,10 +1,15 @@
 #include <stdarg.h>
 #include <stdio.h>
-
+/**
+ * print_all - Sums all its parameters.
+ * @format: aaaaaaa.
+ * Return: The sum of all parameters.
+ * If n is 0, returns 0.
+ */
 void print_all(const char * const format, ...)
 {
 va_list args;
-char * str;
+char *str;
 char aux;
 int i = 0;
 const char *ptr = format;
@@ -12,38 +17,35 @@ char c;
 float f;
 
 va_start(args, format);
- while (format && format[i]) /* chatgpt dice q esto chequea si es nul*/
- {
+while (format && format[i]) /* chatgpt dice q esto chequea si es nul*/
+{
 while (ptr[i])
-        {
+{
 switch (ptr[i])
-            {
-                case 'c':
-                    c = va_arg(args, int);
-                    printf("%c", c);
-                    break;
-                case 'i':
-                    printf("%d", va_arg(args, int));
-                    break;
-                case 'f':
-                    f = (float)va_arg(args, double);
-                    printf("%f", f);
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(nil)";
-                    printf("%s", str);
-                    break;
-}
- 
-    aux = ptr[i];
-    if ((ptr[i + 1]) && (aux == 'c' || aux == 'i' || aux == 'f' || aux ==  's'))
-    printf(", ");
-
- i++;
-        }
-        
+{
+case 'c':
+c = va_arg(args, int);
+printf("%c", c);
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+f = (float)va_arg(args, double);
+printf("%f", f);
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
+break;
+} 
+aux = ptr[i];
+if ((ptr[i + 1]) && (aux == 'c' || aux == 'i' || aux == 'f' || aux ==  's'))
+printf(", ");
+i++;
+}        
 }
 printf("\n");
 }
