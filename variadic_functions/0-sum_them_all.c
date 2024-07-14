2a3e98080b42cb6cf383a3a9cc618cb459d31c66
ariel <ariel2morza@gmail.com>
2024-07-14 14:55:01 +0000

    lk

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,17 +8,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-
+unsigned int i;
+va_list args;
 int sum = 0;
     if (n == 0) {
         return 0;
     }
 
-    va_list args;
+    
     va_start(args, n);
     
     
-    for (unsigned int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         sum = sum + va_arg(args, int);
     }
 
