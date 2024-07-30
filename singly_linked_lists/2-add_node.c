e6a6741b2583f4b1d2816a3ec3743c7921289fed
ariel <ariel2morza@gmail.com>
2024-07-30 17:09:15 +0000

    a

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - This istructure will be used for hub to call the correct
+ * function for each case
+ * @h: The data type as a char
+ * Return: On success 1.
+ * Description: in this structure you will select the best function
+ * for each case.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+  unsigned int add;
+  list_t nuevo;
+  
+  if (str == NULL)
+    {
+      return (NULL);
+    }
+  add = strlen(str);
+  nuevo->len = add;
+  nuevo->str = strdup(str);
+  nuevo->next = head;
+}
