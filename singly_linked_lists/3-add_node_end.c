53e574f9af893f13ae81cde257c9f2502f5dd349
ariel <ariel2morza@gmail.com>
2024-07-30 19:02:15 +0000

    a

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,14 +4,11 @@
 #include "lists.h"
 /**
  * add_node_end - This istructure will be used for hub to call the correct
- * function for each case
  * @head: The data type as a char
  * @str: a
  * Return: On success 1.
- * Description: in this structure you will select the best function
- * for each case.
+ * Description: in this structure you will select the best functi
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 unsigned int add = 0;
@@ -19,29 +16,28 @@ list_t *nuevo;
  list_t *temp;
 const  char *aux;
 if (str == NULL)
-{
 return (NULL);
-}
 nuevo = malloc(sizeof(list_t));
 if (nuevo == NULL)
 {
 free(nuevo);
 return (NULL);
 }
+nuevo->str = strdup(str);
+    if (nuevo->str == NULL)
+    {
+        free(nuevo);
+        return (NULL);
+    }
 aux = str;
 while (*aux != '\0')
 {
-add++;
-aux++;
+add++;aux++;
 }
-
-   nuevo->len = add;
+ nuevo->len = add;
     nuevo->next = NULL;
-
     if (*head == NULL)
-    {
         *head = nuevo;
-    }
     else
     {
         temp = *head;
