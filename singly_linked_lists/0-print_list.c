6658ba9c00af74a181bf44492ea43b4b29c6db3b
ariel <ariel2morza@gmail.com>
2024-07-30 16:45:03 +0000

    a

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,8 +8,12 @@ size_t print_list(const list_t *h){
   size_t contador = 0;
   
   while (h != NULL){
+    if (h->str == NULL)
+      printf("[%d] (nil)\n", h->len);
+      else
     printf("[%d] %s\n",  h->len, h->str);
     h = h->next;
     contador++;
   }
+  return (contador);
 }
