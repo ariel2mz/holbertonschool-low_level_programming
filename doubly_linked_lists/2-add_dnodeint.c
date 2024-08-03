ae85307548f5e5dd005837d7e80fb2fc93501781
ariel <ariel2morza@gmail.com>
2024-08-03 17:38:05 +0000

    qaa

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - This istructure will be used for hub to call the correct
+ * function for each case
+ * @h: The data type as a char
+ * Return: On success 1.
+ * Description: in this structure you will select the best function
+ * for each case.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+unsigned int add = 0;
+list_t *nuevo;
+nuevo = malloc(sizeof(dlistint_t));
+if (nuevo == NULL)
+{
+free(nuevo);
+return (NULL);
+}
+nuevo->n = n;
+nuevo->next = *head;
+head->prev = nuevo;
+*head = nuevo;
+return (nuevo);
+}
