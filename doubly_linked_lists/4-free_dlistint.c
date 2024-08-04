a263b2ab413e7d37e056afc82bb09d5d21c52f0d
ariel <ariel2morza@gmail.com>
2024-08-04 14:24:01 +0000

    asdsa

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -3,9 +3,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_dnodeint_end - This istructure will be used for hub to call the correct
+ * free_dlistint - This istructure will be used for hub to call the correct
  * function for each case
- * @n: The data type as a char
  * @head: aaaaa
  * Return: On success 1.
  * Description: in this structure you will select the best function
