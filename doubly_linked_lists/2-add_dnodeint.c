208ba1209124291092a8ff1013d9bf4122871763
ariel <ariel2morza@gmail.com>
2024-08-04 14:06:36 +0000

    asa

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,26 +2,33 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * add_dnodeint - This istructure will be used for hub to call the correct
- * function for each case
- * @h: The data type as a char
- * Return: On success 1.
- * Description: in this structure you will select the best function
- * for each case.
+ * add_dnodeint - Adds a new node at the beginning of a doubly linked list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: Integer value to store in the new node.
+ * Return: Address of the new node, or NULL if it failed.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *nuevo;
-nuevo = malloc(sizeof(dlistint_t));
-if (nuevo == NULL)
-{
-free(nuevo);
-return (NULL);
-}
-nuevo->n = n;
-nuevo->next = *head;
-head->prev = nuevo;
-*head = nuevo;
-return (nuevo);
+    dlistint_t *nuevo;
+
+    nuevo = malloc(sizeof(dlistint_t));
+    if (nuevo == NULL)
+    {
+        return (NULL);
+    }
+
+    nuevo->n = n;
+    nuevo->prev = NULL;
+    nuevo->next = *head;
+
+    if (*head != NULL)
+    {
+        (*head)->prev = nuevo;
+    }
+
+    *head = nuevo;
+
+    return (nuevo);
 }
